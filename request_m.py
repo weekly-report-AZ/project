f56856f9cc25ff81a354b450188d314b7127a212
Zuev Anton <anton.zuev.1986@gmail.com>
2018-09-27 20:51:22 +0300

    third commit

--- request_m.py
+++ request_m.py
@@ -2,6 +2,6 @@ import requests
 import json
 import settings
 
-s = requests.get("https://api-metrika.yandex.ru/stat/v1/data?dimensions=ym:s:searchEngineName&metrics=ym:s:visits,ym:s:users&filters=ym:s:trafficSourceName=='Переходы из поисковых систем'&id=48348170&oauth_token=AQAAAAAUZLkVAAU3cWSo3bRtVURQrKLg0QzFVL8")
+s = requests.get("https://api-metrika.yandex.ru/stat/v1/data?dimensions=ym:s:lastSearchEngine,ym:s:startURL&metrics=ym:s:visits&filters=ym:s:startURL=@%27keune%27&id=48348170&oauth_token=AQAAAAAUZLkVAAU3cWSo3bRtVURQrKLg0QzFVL8&pretty=true")
 data = s.json()
 print(data)
