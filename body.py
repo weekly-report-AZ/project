f7560b1aa7fc1960252c277e796a37579c29d03e
Zuev Anton <anton.zuev.1986@gmail.com>
2018-10-14 21:34:20 +0300

    тело основного скрипта body переписана, добавлен мультипроцессинг при обращении к api

--- body.py
+++ body.py
@@ -58,3 +58,10 @@ date_2 = end.strftime('%d.%m.%Y %H:%M')
 print(date_2)
 delta = end - begin
 print('время работы скрипта {0} минут {1} секунд'.format(delta.seconds // 60, delta.seconds % 60))
+
+error: failed to push some refs to 'https://github.com/weekly-report-AZ/project.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details
